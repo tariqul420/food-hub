034dce730af080631c3acf783e32ae4bc7453b4b
tariqul420 <tariqul@tariqul.dev>
2026-02-13 09:30:03 +0600

    fix: update backend URL handling in rewrites

--- next.config.ts
+++ next.config.ts
@@ -14,11 +14,11 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/auth/:path*",
-        destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_BACKEND_URL || "https://foodhub-api.tariqul.dev"}/auth/:path*`,
       },
       {
         source: "/api/v1/:path*",
-        destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/v1/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_BACKEND_URL || "https://foodhub-api.tariqul.dev"}/v1/:path*`,
       },
     ];
   },
